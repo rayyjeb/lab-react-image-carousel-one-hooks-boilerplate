96d935496d7b2da2b01f5cf308ec2bf8dcda5a60
rayyjeb <rajab.shoukath@kalvium.community>
2024-01-14 21:42:01 +0530

    hooks

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,40 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [currIndex, setCurrIndex] = useState(0);
+
+  const increaseCount = () => {
+    if (currIndex >= images.length - 1) {
+      setCurrIndex(0);
+    } else {
+      setCurrIndex(currIndex + 1);
+    }
+  };
+
+  const decreaseCount = () => {
+    if (currIndex === 0) {
+      setCurrIndex(images.length - 1);
+    } else {
+      setCurrIndex(currIndex - 1);
+    }
+  };
+
+  return (
+    <div className="carousel-container flex">
+      <div className="leftArrow arrowDiv flex" onClick={decreaseCount}>
+        <ArrowBackIosIcon />
+      </div>
+      <div className="flex">
+        <h2 className="title">{images[currIndex].title}</h2>
+        <img src={images[currIndex].img} alt="" />
+        <h4 className="caption">{images[currIndex].subtitle}</h4>
+      </div>
+      <div className="rightArrow arrowDiv flex" onClick={increaseCount}>
+        <ArrowForwardIosIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
+
